8c5bc86a74745a707f81b3a60f5e722f8fe9c43c
Benenendyel <Vencarlostagaro@gmail.com>
2025-09-27 09:46:43 +0800

    Updated the message when you open a file

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+-- ============================== --
+--          Greet Message         --
+-- ============================== --
+local function welcome_message()
+  print([[
+ __      __       .__                                  __            _______             ____   ____             
+/  \    /  \ ____ |  |   ____  ____   _____   ____   _/  |_  ____    \      \   ____  ___\   \ /   /____   ____  
+\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \  \   __\/  _ \   /   |   \_/ __ \/  _ \   Y   // __ \ /    \ 
+ \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/   |  | (  <_> ) /    |    \  ___(  <_> )     /\  ___/|   |  \
+  \__/\  /  \___  >____/\___  >____/|__|_|  /\___  >  |__|  \____/  \____|__  /\___  >____/ \___/  \___  >___|  /
+       \/       \/          \/            \/     \/                         \/     \/                  \/     \/ 
+  ]])
+end
+
+-- Run on Neovim start
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = welcome_message
+})
+
+
 --------------- For LazyVim Package Manager ----------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -12,6 +33,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
 -- ============================== --
 --      Loading of Configs        --
 -- ============================== --
@@ -19,4 +41,3 @@ require("bendyl.plugins");
 require("bendyl.general-settings");
 require("bendyl.remap");
 
-print("hello, world");
